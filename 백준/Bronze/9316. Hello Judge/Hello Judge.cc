301c644054140ee89ad0c4fe63374c1bc7e2e2d9
Jedo1400 <163664998+Jedo1400@users.noreply.github.com>
2024-05-17 21:06:56 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short n;
+    cin >> n;
+    for (short i = 1; i <= n; ++i)
+        cout << "Hello World, Judge " << i << '!' << '\n';
+}
