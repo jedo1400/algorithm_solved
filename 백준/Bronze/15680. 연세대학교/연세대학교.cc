eedfce058ad5f96b01f3824d711f2dade44ca281
Jedo1400 <163664998+Jedo1400@users.noreply.github.com>
2024-07-18 02:11:27 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    bool n;
+    cin >> n;
+    cout << (n ? "Leading the Way to the Future" : "YONSEI");
+}
