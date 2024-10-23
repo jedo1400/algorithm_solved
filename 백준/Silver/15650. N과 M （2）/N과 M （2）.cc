00d33c18054516506922bd73bb2068217b99a3cc
제도 <163664998+Jedo1400@users.noreply.github.com>
2024-10-23 20:31:37 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+void backtracking(const short &n, const short &m, short depth = 0) {
+    static deque<bool> visited(n+1, 0);
+    static vector<short> result(m);
+
+    if (depth == m) {
+        for (auto &i : result)
+            cout << i << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (short i = 1; i <= n; ++i) {
+        if (!visited[i] && i > (depth ? result[depth-1] : 0)) {
+            visited[i] = 1;
+            result[depth] = i;
+            backtracking(n, m, depth+1);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() {
+    short n, m;
+    cin >> n >> m;
+    backtracking(n, m);
+}
