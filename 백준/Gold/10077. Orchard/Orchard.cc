03d83d553402feafaacc840bbeda735d307d84a9
제도 <163664998+jedo1400@users.noreply.github.com>
2025-05-03 21:31:54 +0900

    [Gold V] Title: Orchard, Time: 180 ms, Memory: 25584 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10077. Orchard/Orchard.cc
+++ 백준/Gold/10077. Orchard/Orchard.cc
@@ -1,16 +1,20 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+#define LL long long
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 	cout.tie(NULL);
 
-    int n, m, total = 0;
+    int n, m;
+    LL total = 0;
     cin >> n >> m;
-    vector<vector<int>> sum(n+1, vector<int>(m+1, 0));
+    vector<vector<LL>> sum(n+1, vector<LL>(m+1, 0));
 
     for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= m; ++j) {
@@ -18,23 +22,29 @@ int main(){
             cin >> t;
             total += t;
             
-            sum[i][j] = t + sum[i][j-1];
+            sum[i][j] = t + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1];
         }
 
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j <= m; ++j)
-            sum[i][j] += sum[i-1][j];
+    LL smallest = 0;
+    vector<LL> arr(m+1); // 비용에 대한 기여도 작을 수록 좋음
 
-    int result = 1e9;
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j <= m; ++j)
-            for (int k = 0; i+k <= n; ++k)
-                for (int l = 0; j+l <= m; ++l) {
-                    int mc = j+l, mr = i+k
-                        , prefix_sum = sum[mr][mc] - sum[i-1][mc] - sum[mr][j-1] + sum[i-1][j-1];
-                    
-                    result = min(result, (k+1)*(l+1) + total - prefix_sum * 2);
-                }
+    for (int t = 1; t <= n; ++t) {
+        for (int b = t; b <= n; ++b) {
+            int h = b - t + 1; // 높이
 
-    cout << result;
+            for (int j = 1; j <= m; ++j) // 최적인 열 찾기
+                arr[j] = h - 2 * (sum[b][j] - sum[t-1][j] - sum[b][j-1] + sum[t-1][j-1]);
+
+            LL candidate = arr[1], cur = arr[1];
+
+            // 최소 연속부분합
+            for(int j = 2; j <= m; ++j)
+                cur = min(arr[j], cur + arr[j])
+                , candidate = min(candidate, cur);
+
+            smallest = min(smallest, candidate);
+        }
+    }
+
+    cout << smallest + total << "\n";
 }
